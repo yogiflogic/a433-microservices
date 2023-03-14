ee5a341cb6dd4ed766f7ff63970b6d4ea348f202
Yogi Fernando <yogiflogic79@gmail.com>
2023-03-15 02:03:44 +0700

    update file build

--- build_push_image.sh
+++ build_push_image.sh
@@ -4,12 +4,12 @@ sudo docker build -t item-app:v1 .
 # Melihat list docker images di local
 sudo docker images
 
-# Mengubah nama image sesuai dengan repsitory di dockerhub / GitHub Packages
+# Mengubah nama image sesuai dengan repsitory di GitHub Packages
 sudo docker tag item-app:v1 ghcr.io/yogiflogic/item-app:v1
 
 # login ke github container registry
 # ketik di terminal "export PASSWORD_DOCKER_HUB=<password_Anda>"
 echo $CR_PAT | docker login ghcr.io -u yogiflogic --password-stdin
 
-# mengunggah image ke docker hub
+# mengunggah image ke github container registry
 sudo docker push ghcr.io/yogiflogic/item-app:v1
