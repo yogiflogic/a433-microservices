bbf2bf5667fb142279d1bd5d5afe0fd33f2bb975
Yogi Fernando <yogiflogic79@gmail.com>
2023-03-24 14:50:38 +0700

    menambahkan script sh karsa jobs

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,15 @@
+#  Membuat Docker Images
+sudo docker build -t karsajobs:latest .
+
+# Melihat list docker images di local
+sudo docker images
+
+# Mengubah nama image sesuai dengan repsitory di GitHub Packages
+sudo docker tag karsajobs:latest ghcr.io/yogiflogic/karsajobs:latest
+
+# login ke github container registry
+# ketik di terminal "export PASSWORD_DOCKER_HUB=<password_Anda>"
+echo $CR_PAT | docker login ghcr.io -u yogiflogic --password-stdin
+
+# mengunggah image ke github container registry
+sudo docker push ghcr.io/yogiflogic/karsajobs:latest
