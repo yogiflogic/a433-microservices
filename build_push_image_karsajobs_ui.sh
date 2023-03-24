80193b3b522ee8d59c8ed4d26a41ce4fef137b44
Yogi Fernando <yogiflogic79@gmail.com>
2023-03-24 23:39:04 +0700

    membuat file script build

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,15 @@
+#  Membuat Docker Images
+sudo docker build -t karsajobs-ui:latest .
+
+# Melihat list docker images di local
+sudo docker images
+
+# Mengubah nama image sesuai dengan repsitory di GitHub Packages
+sudo docker tag karsajobs-ui:latest ghcr.io/yogiflogic/karsajobs-ui:latest
+
+# login ke github container registry
+# ketik di terminal "export CR_PAT=YOUR_TOKEN"
+echo $CR_PAT | docker login ghcr.io -u yogiflogic --password-stdin
+
+# mengunggah image ke github container registry
+sudo docker push ghcr.io/yogiflogic/karsajobs-ui:latest
