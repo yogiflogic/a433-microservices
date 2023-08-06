49e9bebfcd88ce95708da7a90d88967269236073
Yogi Fernando <yogiflogic79@gmail.com>
2023-08-06 15:42:33 +0700

    Update build_push_image.sh
    update how to login with github package

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,7 +7,9 @@ sudo docker images
 # Mengubah nama image sesuai dengan repsitory di GitHub Packages
 sudo docker tag item-app:v1 ghcr.io/yogiflogic/item-app:v1
 
-# login ke github container registry
+# login jika menggunakan github package
+# ketik di terminal "export CR_PAT=YOUR_TOKEN"
+# login jika menggunakan docker hub
 # ketik di terminal "export PASSWORD_DOCKER_HUB=<password_Anda>"
 echo $CR_PAT | docker login ghcr.io -u yogiflogic --password-stdin
 
